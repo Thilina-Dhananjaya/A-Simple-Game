46d809dec07804f11ec579ad01e3a14e2d578b21
Thilina-Dhananjaya <120640837+Thilina-Dhananjaya@users.noreply.github.com>
2024-03-25 11:14:07 +0530

    person created

---
+++ A Simple Game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
